7584390b575e9026d483ccd8992848cc14fd9128
Esranur AYGÜN <d76gyms4yn@privaterelay.appleid.com>
2026-07-22 20:32:55 +0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: ""https://crm-system-rz6n.onrender.com/api",
+  baseURL: "https://crm-system-rz6n.onrender.com",
 });
 
 // Attach token to every request automatically
